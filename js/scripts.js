6e302cd6172b137213b4a33432b5367d73f771b9
Guest User <Guest@Epicodus-9F.local>
2017-08-18 09:23:04 -0700

    pass first spec counting numbers up to input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 //Business Logic
-
+function numberReturn(number) {
+  numberArr = []
+  for (i=1; i <= number; i++) {
+    numberArr.push(i);
+  }
+  console.log(numberArr);
+  return numberArr
+}
 
 //UI Logic
+$(document).ready(function() {
+  $("form#submitNumber").submit(function(event) {
+    event.preventDefault();
+    var input = $("input#number").val();
+
+    $("#output").text(numberReturn(input));
+  });
+});
